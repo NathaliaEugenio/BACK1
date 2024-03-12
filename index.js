2834e482a9b3b2031a5fca54f3cf0591740a62c0
Nathalia Eugenio <eugenionathalia@gmail.com>
2024-03-12 14:59:18 -0300

    abrir arquivos com fs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import fs from 'fs';
+
 import chalk from 'chalk';
 
-console.log(chalk.blue('Olá mundo'));
+
+/*console.log(chalk.blue('Olá mundo'));
 
 console.log(chalk.blue.bgWhite.bold("SENAI"));
 
@@ -13,3 +16,26 @@ CPU: ${chalk.red('90%')}
 RAM: ${chalk.green('40%')}
 DISK: ${chalk.yellow('70%')}
 `);
+*/
+
+//2)função qur trata erros
+
+function trataErro(erro){
+    throw new Error(chalk.red(erro));
+}
+
+
+//1)escrever uma function que traga arquivos .md
+function pegaArquivo(caminhoDoArquivo){
+    const encoding="utf-8";
+    fs.readFile(caminhoDoArquivo,encoding,(_erro,texto)=>{
+        if(erro){
+            trataErro(erro)
+        }
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md')
+
+//callback(erro,texto)
